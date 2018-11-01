cfd416ed614e2b92e0374be1bd6a9414cd0dd279
ReversonMendes <reversondv@gmail.com>
2018-11-01 01:11:25 -0300

    novo layout

--- js/init.js
+++ js/init.js
@@ -6,3 +6,15 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+$(document).ready(function(){
+  $('.sidenav').sidenav();
+});
+
+$('.button-collapse').sideNav({
+  menuWidth: 300, // Default is 300
+  edge: 'left', // Choose the horizontal origin
+  closeOnClick: false, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+  draggable: true // Choose whether you can drag to open on touch screens,
+}
+);
